8e638df712c092171876ca8fb6f34bd8ab2261c3
SoumyadipOjha <soumyadip.ojha@lpu.in>
2024-02-03 02:53:12 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (11.30%) - LeetSync

---
+++ 162-find-peak-element/find-peak-element.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int max=INT_MIN;int idx=0;
+        for(int i=0;i<nums.size();i++){
+            if(max<nums[i]){
+                max=nums[i];
+                idx=i;
+            }
+        }
+        return idx;
+    }
+};
