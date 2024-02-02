06fce20f23efc746f7fb86d18d4fae4a27f7a25e
SoumyadipOjha <soumyadip.ojha@lpu.in>
2024-02-02 12:48:50 +0530

    Time: 0 ms (100.00%) | Memory: 7.2 MB (13.81%) - LeetSync

--- 1212-sequential-digits/sequential-digits.cpp
+++ 1212-sequential-digits/sequential-digits.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     vector<int> sequentialDigits(int low, int high) {
-        vector<int> v, ans;
-        for (int i = 1; i <=9; i++) {
+        vector<int> result;
+        for (int i = 1; i <= 9; i++) {
             int num = i;
-            for (int j =i+ 1; j <=9; j++) {
+            for (int j = i + 1; j <= 9; j++) {
                 num = num * 10 + j;
-                v.push_back(num);
+
+                if (num >= low && num <= high) {
+                    result.push_back(num);
+                }
             }
         }
-        sort(v.begin(),v.end());
-        for (int i = 0; i < v.size(); i++) {
-            if (v[i] >= low && v[i] <= high) {
-                ans.push_back(v[i]);
-            }
-        }
-        return ans;
+        sort(result.begin(),result.end());
+
+        return result;
     }
 };
