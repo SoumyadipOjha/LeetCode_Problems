9353346d6fcb87eb2506cbea825382d6e5b78173
SoumyadipOjha <soumyadip.ojha@lpu.in>
2024-02-13 12:44:44 +0530

    Time: 63 ms (11.29%) | Memory: 29.9 MB (7.01%) - LeetSync

--- 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.cpp
+++ 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.cpp
@@ -1,21 +1,10 @@
 class Solution {
 public:
     string firstPalindrome(vector<string>& words) {
-        // string ans="";
-        // for(int i=0;i<words.size();i++){
-        //     string temp=words[i];
-        //     for(int j=0;j<temp.size()/2;j++){
-        //         for(int k=temp.size();k>temp.size()/2;k--){
-        //             if(temp[i]==temp[j]){
-        //                 ans=temp;
-        //                 break;
-        //             }
-        //         }
-        //     }
-        // }
-        // return ans;
+    
         
-        for(int i=0;i<words.size();i++){
+        int n=words.size();
+        for(int i=0;i<n;i++){
             string temp1=words[i];
             string temp2=words[i];
             reverse(temp1.begin(),temp1.end());
