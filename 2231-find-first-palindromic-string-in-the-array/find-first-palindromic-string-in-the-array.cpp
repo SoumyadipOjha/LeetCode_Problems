a1bddf73f1be6792f1ab728d41aaea4742802a01
SoumyadipOjha <soumyadip.ojha@lpu.in>
2024-02-13 12:34:47 +0530

    Time: 61 ms (13.54%) | Memory: 29.8 MB (10.41%) - LeetSync

--- 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.cpp
+++ 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.cpp
@@ -15,8 +15,7 @@ public:
         // }
         // return ans;
         
-        int n=words.size();
-        for(int i=0;i<n;i++){
+        for(int i=0;i<words.size();i++){
             string temp1=words[i];
             string temp2=words[i];
             reverse(temp1.begin(),temp1.end());
