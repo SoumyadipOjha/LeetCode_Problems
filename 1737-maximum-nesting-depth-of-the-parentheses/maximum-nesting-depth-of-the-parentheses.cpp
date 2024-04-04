42b74acc86068a44de49b006bacf7089fb34ef76
SoumyadipOjha <soumyadip.ojha@lpu.in>
2024-04-04 19:52:58 +0530

    Time: 3 ms (40.00%) | Memory: 7.3 MB (78.96%) - LeetSync

--- 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int maxDepth(string s) {
+        if(s=="")return 0;
         int count=0;int maxi=0;
         for(int i=0;i<s.size();i++){
            if(s[i]=='('){
             count++;
             maxi=max(maxi,count);
            }
-           if(s[i]==')'){
+           if(s[i]==')')
             count--;
-           }
            
         }
         return maxi;
