4b8e1475242dcee6dd170d2aec1582e25cb05742
SoumyadipOjha <soumyadip.ojha@lpu.in>
2024-06-03 11:26:40 +0530

    Time: 15 ms (87.56%) | Memory: 11.9 MB (83.42%) - LeetSync

---
+++ 2572-append-characters-to-string-to-make-subsequence/append-characters-to-string-to-make-subsequence.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int appendCharacters(string s, string t) {
+        int j=0;
+        for(int i=0;i<s.size();i++){
+            if(j<t.size() && s[i]==t[j]){
+                j++;
+            }
+        }
+        return t.size()-j;
+    }
+};
