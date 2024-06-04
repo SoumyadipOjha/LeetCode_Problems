0661bab64e9d29d0f711f1521cc76b8d57f04db9
SoumyadipOjha <soumyadip.ojha@lpu.in>
2024-06-04 12:40:42 +0530

    Time: 48 ms (40.36%) | Memory: 12.6 MB (74.11%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -4,7 +4,7 @@ public:
         for(int i=0;i<nums.size();i++){
             for(int j=i+1;j<nums.size();j++){
                 if(nums[i]+nums[j]==target){
-                   return{i,j};
+                   return {i,j};
                 }
             }
         }
