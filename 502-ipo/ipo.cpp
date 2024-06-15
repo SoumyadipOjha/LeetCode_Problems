f34e94175d09d5edfd46f264a3512f73e0f8baa0
SoumyadipOjha <soumyadip.ojha@lpu.in>
2024-06-15 11:48:41 +0530

    Time: 108 ms (97.62%) | Memory: 84.4 MB (74.99%) - LeetSync

---
+++ 502-ipo/ipo.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int findMaximizedCapital(int k, int w, vector<int>& profits,
+                             vector<int>& capital) {
+        ios_base::sync_with_stdio(false);
+        int n = profits.size();
+        vector<pair<int, int>> minCapital;
+
+        for (int i = 0; i < n; i++)
+            minCapital.push_back({capital[i], profits[i]});
+
+        sort(minCapital.begin(), minCapital.end());
+
+        priority_queue<int> maxProfit;
+        int j = 0;
+        for (int i = 0; i < k; ++i) {
+            while (j < n && minCapital[j].first <= w) {
+                maxProfit.push(minCapital[j].second);
+                j++;
+            }
+
+            if (maxProfit.empty())
+                break;
+
+            w += maxProfit.top();
+            maxProfit.pop();
+        }
+
+        return w;
+    }
+};
