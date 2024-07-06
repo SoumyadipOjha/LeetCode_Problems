456e9658799dae5c0dc189eea0182f6ad6dd2533
SoumyadipOjha <soumyadip.ojha@lpu.in>
2024-07-06 15:55:06 +0530

    Time: 2 ms (54.14%) | Memory: 7.1 MB (30.58%) - LeetSync

---
+++ 2645-pass-the-pillow/pass-the-pillow.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int passThePillow(int n, int time) {
+        if (time < n)
+            return time + 1;
+
+        int q = time / (n - 1);
+        if (q % 2 == 0) {
+            return 1 + time % (n - 1);
+        }
+
+        return n - time % (n - 1);
+    }
+};
