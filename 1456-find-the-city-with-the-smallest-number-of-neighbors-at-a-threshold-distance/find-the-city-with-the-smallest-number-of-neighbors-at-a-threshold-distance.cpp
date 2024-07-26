edb346a01537399d8429ac7963e87612b01527db
SoumyadipOjha <soumyadip.ojha@lpu.in>
2024-07-26 14:17:56 +0530

    Time: 20 ms (92.28%) | Memory: 15.5 MB (79.53%) - LeetSync

---
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+        vector<vector<int>> dist(n, vector<int>(n, 10001));
+
+        // Initialize distance matrix
+        for (int i = 0; i < n; i++) {
+            dist[i][i] = 0;
+        }
+
+        // Build initial graph
+        for (const auto& edge : edges) {
+            dist[edge[0]][edge[1]] = edge[2];
+            dist[edge[1]][edge[0]] = edge[2];
+        }
+
+        // Floyd-Warshall algorithm
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+                }
+            }
+        }
+
+        int minReachableCities = n;
+        int result = -1;
+
+        // Find the city with the smallest number of reachable cities
+        for (int i = 0; i < n; i++) {
+            int reachableCities = 0;
+            for (int j = 0; j < n; j++) {
+                if (dist[i][j] <= distanceThreshold) {
+                    reachableCities++;
+                }
+            }
+            if (reachableCities <= minReachableCities) {
+                minReachableCities = reachableCities;
+                result = i;
+            }
+        }
+
+        return result;
+    }
+};
