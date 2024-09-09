96734778ca94e8c27243f2c8e0672827d8bc8bb0
SoumyadipOjha <soumyadip.ojha@lpu.in>
2024-09-09 11:03:05 +0530

    Time: 4 ms (80.01%) | Memory: 9 MB (55.31%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -2,19 +2,18 @@ class Solution {
 public:
     bool isPalindrome(string s) {
         int left = 0;
-        int right = s.size()-1;
-        while (left<right) {
+        int right = s.size() - 1;
+        while (left < right) {
             if (!isalnum(s[left])) {
                 left++;
                 continue;
             }
-            if (!isalnum(s[right])){
+            if (!isalnum(s[right])) {
                 right--;
                 continue;
             }
-            if(tolower(s[left])!=tolower(s[right])){
+            if (tolower(s[left]) != tolower(s[right]))
                 return false;
-            }
             left++;
             right--;
         }
