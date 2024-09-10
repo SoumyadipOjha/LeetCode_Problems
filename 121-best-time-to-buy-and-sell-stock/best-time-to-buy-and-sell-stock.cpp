38e9b0fba3df63e574c3ea5b68e9357898af886a
SoumyadipOjha <soumyadip.ojha@lpu.in>
2024-09-10 23:32:00 +0530

    Time: 74 ms (91.76%) | Memory: 96.1 MB (90.41%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+
+    int maxProfit(vector<int>& prices) {
+        if(prices.size()<2)return 0;
+     int max_diff=0;
+     int min=prices[0];
+     for(int i=1;i<prices.size();i++){
+        if(prices[i]-min>max_diff)max_diff=prices[i]-min;
+        if(prices[i]<min)min=prices[i];
+     }   
+     return max_diff;
+    }
+};
