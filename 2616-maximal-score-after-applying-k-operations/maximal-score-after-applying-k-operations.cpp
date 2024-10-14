ffb649d4c6c0fefb70650f45d34471f76a78364b
SoumyadipOjha <soumyadip.ojha@lpu.in>
2024-10-14 18:52:57 +0530

    Time: 393 ms (6.98%) | Memory: 85.9 MB (14.96%) - LeetSync

--- 2616-maximal-score-after-applying-k-operations/maximal-score-after-applying-k-operations.cpp
+++ 2616-maximal-score-after-applying-k-operations/maximal-score-after-applying-k-operations.cpp
@@ -1,15 +1,27 @@
 class Solution {
 public:
     long long maxKelements(vector<int>& nums, int k) {
+        // priority_queue<long long>pq;
+        // for(auto &it:nums)pq.push(it);
+        // long long score=0;
+        // long double temp;
+        // while(k--){
+        //     temp=pq.top();
+        //     score+=temp;
+        //     pq.pop();
+        //     pq.push(ceil(temp/3));
+        // }
+        // return score;
         priority_queue<long long>pq;
-        for(auto &it:nums)pq.push(it);
+        for(auto&it:nums)pq.push(it);
         long long score=0;
-        long double temp;
+        long double temp=0;
         while(k--){
             temp=pq.top();
             score+=temp;
             pq.pop();
             pq.push(ceil(temp/3));
+
         }
         return score;
     }
