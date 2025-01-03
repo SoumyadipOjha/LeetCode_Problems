e3aa035c995c6302d0013ece02a74b5e7f10fc12
SoumyadipOjha <soumyadip.ojha@lpu.in>
2025-01-03 18:57:06 +0530

    Time: 0 ms (100.00%) | Memory: 14.3 MB (66.14%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -1,24 +1,38 @@
 class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
-        int m = matrix.size(), n = matrix[0].size();
-        if (m == 1) {
-            return *min_element(matrix[0].begin(), matrix[0].end());
-        }
+        // int m = matrix.size(), n = matrix[0].size();
+        // if (m == 1) {
+        //     return *min_element(matrix[0].begin(), matrix[0].end());
+        // }
 
+        // for (int i = 1; i < m; i++) {
+        //     for (int j = 0; j < n; j++) {
+        //         int min_val = matrix[i - 1][j];
+        //         if (j > 0) {
+        //             min_val = min(min_val, matrix[i - 1][j - 1]);
+        //         }
+        //         if (j < n - 1) {
+        //             min_val = min(min_val, matrix[i - 1][j + 1]);
+        //         }
+        //         matrix[i][j] += min_val;
+        //     }
+        // }
+
+        // return *min_element(matrix[m - 1].begin(), matrix[m - 1].end());
+        int m = matrix.size(), n = matrix[0].size();
+        if (m == 1)
+            return *min_element(matrix[0].begin(), matrix[0].end());
         for (int i = 1; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                int min_val = matrix[i - 1][j];
-                if (j > 0) {
-                    min_val = min(min_val, matrix[i - 1][j - 1]);
-                }
-                if (j < n - 1) {
-                    min_val = min(min_val, matrix[i - 1][j + 1]);
-                }
-                matrix[i][j] += min_val;
+                int minV = matrix[i - 1][j];
+                if (j <n-1)
+                    minV = min(minV, matrix[i - 1][j + 1]);
+                if (j > 0)
+                    minV = min(matrix[i - 1][j -1], minV);
+                matrix[i][j] += minV;
             }
         }
-
-        return *min_element(matrix[m - 1].begin(), matrix[m - 1].end());
+        return *min_element(matrix[n - 1].begin(), matrix[n - 1].end());
     }
 };
