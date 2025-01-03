abe8053c4e61ffb947b32fbe35b60657ec3e3366
SoumyadipOjha <soumyadip.ojha@lpu.in>
2025-01-03 18:45:00 +0530

    Time: 0 ms (100.00%) | Memory: 14.2 MB (68.65%) - LeetSync

---
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        int m = matrix.size(), n = matrix[0].size();
+        if (m == 1) {
+            return *min_element(matrix[0].begin(), matrix[0].end());
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int min_val = matrix[i - 1][j];
+                if (j > 0) {
+                    min_val = min(min_val, matrix[i - 1][j - 1]);
+                }
+                if (j < n - 1) {
+                    min_val = min(min_val, matrix[i - 1][j + 1]);
+                }
+                matrix[i][j] += min_val;
+            }
+        }
+
+        return *min_element(matrix[m - 1].begin(), matrix[m - 1].end());
+    }
+};
