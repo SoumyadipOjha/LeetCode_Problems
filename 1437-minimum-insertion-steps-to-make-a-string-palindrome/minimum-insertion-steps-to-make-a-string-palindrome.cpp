eed0ebaa077b682bb89f7a74f3a78dac11ba9059
SoumyadipOjha <soumyadip.ojha@lpu.in>
2025-01-06 02:21:00 +0530

    Time: 13 ms (97.41%) | Memory: 9.7 MB (90.15%) - LeetSync

---
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int longest = 0;
+        vector<int> dp(text2.length(), 0);
+        for (char c : text1) {
+            int curr = 0;
+            for (int i = 0; i < dp.size(); i++) {
+                if (curr < dp[i])
+                    curr = dp[i];
+                else if (text2[i] == c)
+                    dp[i] = curr + 1;
+
+                longest = max(longest, dp[i]);
+            }
+        }
+        return longest;
+    }
+    int minInsertions(string s) {
+        string revS = s;
+        reverse(revS.begin(), revS.end());
+        int a = longestCommonSubsequence(s, revS);
+        return s.size()-a;
+    }
+};
