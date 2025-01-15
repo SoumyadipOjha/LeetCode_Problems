4dac7439087a852bfe3bd7aca1d29353c2a6937d
SoumyadipOjha <soumyadip.ojha@lpu.in>
2025-01-15 19:12:28 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (28.78%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     int climbStairs(int n) {
+        // memo
+        // vector<int>dp(n+1,-1);
+        // if(n<=2)return n;
+        // if(dp[n]!=-1)return dp[n];
+        // return dp[n]=climbStairs(n-1)+climbStairs(n-2);
+        // teabu
         vector<int> dp(n + 1, 0);
         dp[0] = 1;
         dp[1] = 1;
