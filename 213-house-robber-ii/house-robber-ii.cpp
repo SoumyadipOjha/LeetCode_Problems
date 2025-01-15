91f67a2496da96c2a02f73672717cf718e08cc21
SoumyadipOjha <soumyadip.ojha@lpu.in>
2025-01-15 20:50:26 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (35.25%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-int rob1(vector<int>&nums){
-    if(nums.size()==1)return nums[0];
-    vector<int>dp(nums.size());
-    dp[0]=nums[0];
-    dp[1]=max(nums[0],nums[1]);
-    for(int i=2;i<nums.size();i++){
-        dp[i]=max(nums[i]+dp[i-2],dp[i-1]);
+    int rob1(vector<int>& nums) {
+        if (nums.size() == 1)
+            return nums[0];
+        vector<int> dp(nums.size(), 0);
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+        for (int i = 2; i < nums.size(); i++) {
+            dp[i] = max(dp[i - 2] + nums[i], dp[i - 1]);
+        }
+        return dp[nums.size() - 1];
     }
-    return dp[nums.size()-1]; 
-}
     int rob(vector<int>& nums) {
-      if(nums.size()==1)return nums[0];
-
-      vector<int>skipLast(nums.begin(),nums.end()-1);  
-      vector<int>skipFirst(nums.begin()+1,nums.end());  
-      
-      int skipLastAns=rob1(skipLast);
-      int skipFirstAns=rob1(skipFirst);
-      return max(skipLastAns,skipFirstAns);
+        if (nums.size() == 1)
+            return nums[0];
+        vector<int> skipFirst(nums.begin() + 1, nums.end());
+        vector<int> skipLast(nums.begin(), nums.end() - 1);
+        int s1 = rob1(skipFirst);
+        int s2 = rob1(skipLast);
+        return max(s1, s2);
     }
 };
