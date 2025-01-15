9c79c7bc5ce14b1dfaf7df2434482985d65e0795
SoumyadipOjha <soumyadip.ojha@lpu.in>
2025-01-15 19:16:08 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (73.19%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,18 +1,38 @@
 class Solution {
 public:
     int climbStairs(int n) {
+        // recur
+        // if(n<=2)return n;
+        // return dp[n]=climbStairs(n-1)+climbStairs(n-2);
+
         // memo
         // vector<int>dp(n+1,-1);
         // if(n<=2)return n;
         // if(dp[n]!=-1)return dp[n];
         // return dp[n]=climbStairs(n-1)+climbStairs(n-2);
+
         // teabu
-        vector<int> dp(n + 1, 0);
-        dp[0] = 1;
-        dp[1] = 1;
-        for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
+        // vector<int> dp(n + 1, 0);
+        // dp[0] = 1;
+        // dp[1] = 1;
+        // for (int i = 2; i <= n; i++) {
+        //     dp[i] = dp[i - 1] + dp[i - 2];
+        // }
+        // return dp[n];
+
+        // space opt
+        if (n <= 0)
+            return 0; 
+        if (n == 1)
+            return 1;
+        if (n == 2)
+            return 2;
+        int p1 = 1, p2 = 2;
+        for (int i = 3; i <= n; i++) {
+            int ans = p1 + p2;
+            p1 = p2;
+            p2 = ans;
         }
-        return dp[n];
+        return p2;
     }
 };
