87cd86a889810751d06ec3a668c23052b1f55dec
SoumyadipOjha <soumyadip.ojha@lpu.in>
2025-01-19 23:22:31 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (28.44%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,7 +1,24 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+
+        if(nums.size()==0)cout<<"";
+        int min=nums[0],max=nums[0];
+        for(int i:nums){
+            if(i<min)min=i;
+            if(i>max)max=i;
+        }
+        int range=max-min+1;
+        vector<int>count(range,0);
+        for(int i:nums)count[i-min]++;
+
+        int index=0;
+        for(int i=0;i<range;i++){
+            while(count[i]>0){
+                nums[index++]=i+min;
+                count[i]--;
+            }
+        }
         for(int i=0;i<nums.size();i++){
             cout<<nums[i];
         }
