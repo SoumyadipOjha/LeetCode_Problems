925b9eb8fbd5b506f3b1b5467acc9495ba9b770b
SoumyadipOjha <soumyadip.ojha@lpu.in>
2025-01-20 23:05:13 +0530

    Time: 83 ms (5.66%) | Memory: 23.9 MB (46.49%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int left = 0;
-        int n = nums.size();
-        for (int i = 0; i < n; i++) {
-            if (nums[i] != 0) {
-                swap(nums[left], nums[i]);
-                left++;
+        int start=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=0){
+                swap(nums[i],nums[start]);
+                start++;
             }
         }
+       for(int i=0;i<nums.size();i++){
+        cout<<nums[i];
+       }
     }
 };
