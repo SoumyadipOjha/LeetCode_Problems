7f1f2e04c6ee25aa7beeb19bf7b35f117584c072
SoumyadipOjha <soumyadip.ojha@lpu.in>
2025-02-12 00:27:11 +0530

    Time: 0 ms (100.00%) | Memory: 16.8 MB (66.09%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,50 +1,66 @@
 class Solution {
 public:
-    bool isValid(int i, int j, int m, int n, vector<vector<int>>& grid) {
+    bool isValid(int i, int j, int n, int m, vector<vector<int>>& grid) {
         return (i >= 0 && i < n && j >= 0 && j < m && grid[i][j] == 1);
     }
+int orangesRotting(vector<vector<int>>& grid) {
+    int n = grid.size();
+    int m = grid[0].size();
+    int fresh = 0;
+    queue<pair<int, int>> q;
+    int time = 0;
 
-    int orangesRotting(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-        queue<pair<int, int>> q;
-        int freshCount = 0, time = 0;
-
-        // Directions for up, down, left, right
-        vector<pair<int, int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-
-        // Push all initially rotten oranges into the queue
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 2) {
-                    q.push({i, j});
-                } else if (grid[i][j] == 1) {
-                    freshCount++;
-                }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (grid[i][j] == 2) {
+                // push into queue
+                q.push({i, j});
+            } else if (grid[i][j] == 1) {
+                fresh++;
             }
         }
-
-        // BFS traversal
-        while (!q.empty() && freshCount > 0) {
-            int size = q.size();
-            time++; // Increment time after processing one level of BFS
-
-            for (int k = 0; k < size; k++) {
-                auto [x, y] = q.front();
-                q.pop();
-
-                for (auto [dx, dy] : directions) {
-                    int nx = x + dx, ny = y + dy;
-
-                    if (isValid(nx, ny, m, n, grid)) {
-                        grid[nx][ny] = 2; // Mark orange as rotten
-                        freshCount--;
-                        q.push({nx, ny});
-                    }
-                }
-            }
-        }
-
-        return (freshCount == 0) ? time : -1;
     }
-};
+    if (fresh == 0)
+        return 0;
+    // start bfs trav
+    while (!q.empty()) {
+        int size_q = q.size();
+        int temp = 0;
+        while (size_q) {
+            pair<int, int> p = q.front();
+            q.pop();
+            int x1 = p.first;
+            int y1 = p.second;
+            int ax[4] = {1, -1, 0, 0};
+            int ay[4] = {0, 0, 1, -1};
+            for (int i = 0; i < 4; i++) {
+                int x = ax[i] + x1;
+                int y = ay[i] + y1;
+
+                if (isValid(x, y, n, m, grid)) {
+                    temp++;
+                    grid[x][y] = 2;
+                    q.push({x, y});
+                }
+            }
+            size_q--;
+        }
+        if (temp != 0) {
+            time++;
+        }
+    }
+
+    // check if there is any fresh orange after bfs
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (grid[i][j] == 1) {
+                time = 0;
+                break;
+            }
+        }
+    }
+
+    return (time == 0) ? -1 : time;
+}
+}
+;
