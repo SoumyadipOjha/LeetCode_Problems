dbf74a3039a20c3f9a39513fa12688189a4424b4
SoumyadipOjha <soumyadip.ojha@lpu.in>
2025-02-19 17:37:37 +0530

    Time: 26 ms (64.04%) | Memory: 27.4 MB (50.82%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     int longestCommonSubsequence(string text1, string text2) {
-        int longest = 0;
-        vector<int> dp(text2.size(), 0);
-        for (char c : text1) {
-            int curr = 0;
-            for (int i = 0; i < dp.size(); i++) {
-                if (curr < dp[i])
-                    curr = dp[i];
-                else if (text2[i] == c)
-                    dp[i] = curr + 1;
-                longest = max(longest, dp[i]);
+        int n=text1.size();
+        int m=text2.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(text1[i-1]==text2[j-1])dp[i][j]=1+dp[i-1][j-1];
+                else{
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
             }
         }
-        return longest;
+        return dp[n][m];
     }
 };
