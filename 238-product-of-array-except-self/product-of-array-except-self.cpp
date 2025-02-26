be5880cc30025e943568c4e1667c34b85eaee402
SoumyadipOjha <soumyadip.ojha@lpu.in>
2025-02-26 15:27:41 +0530

    Time: 3 ms (39.04%) | Memory: 42.3 MB (16.80%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,19 +1,28 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-       vector<int>l;
-       l.push_back(1);
-       for(int i=0;i<nums.size();i++){
-        l.push_back(l[i]*nums[i]);
-       }
-       int product=1;
-       for(int i=nums.size()-1;i>0;i--){
-        int initial=nums[i];
-        nums[i]=l[i]*product;
-        product=product*initial;
-       }
-       nums[0]=product;
-       return nums;
+        int n = nums.size();
+        vector<int> pre(n, 1), suf(n, 1), ans(n, 1);
 
+        // Compute prefix products
+        pre[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            pre[i] = pre[i - 1] * nums[i];
+        }
+
+        // Compute suffix products
+        suf[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            suf[i] = suf[i + 1] * nums[i];
+        }
+
+        // Compute result using prefix and suffix products
+        for (int i = 0; i < n; i++) {
+            int left = (i > 0) ? pre[i - 1] : 1;
+            int right = (i < n - 1) ? suf[i + 1] : 1;
+            ans[i] = left * right;
+        }
+
+        return ans;
     }
 };
