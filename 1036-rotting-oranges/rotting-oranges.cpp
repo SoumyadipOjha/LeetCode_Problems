8678c394dc9aab20f7c5a71e2721413cb746fca4
SoumyadipOjha <soumyadip.ojha@lpu.in>
2025-02-27 03:14:20 +0530

    Time: 0 ms (100.00%) | Memory: 16.7 MB (66.18%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,66 +1,49 @@
 class Solution {
 public:
-    bool isValid(int i, int j, int n, int m, vector<vector<int>>& grid) {
-        return (i >= 0 && i < n && j >= 0 && j < m && grid[i][j] == 1);
-    }
-int orangesRotting(vector<vector<int>>& grid) {
-    int n = grid.size();
-    int m = grid[0].size();
-    int fresh = 0;
-    queue<pair<int, int>> q;
-    int time = 0;
+    int r, c;
+    int row[4] = {-1, +1, 0, 0};
+    int col[4] = {0, 0, -1, +1};
+    bool isValid(int i, int j) { return (i >= 0 && i < r && j >= 0 && j < c); }
+    int orangesRotting(vector<vector<int>>& grid) {
+        r = grid.size();
+        c = grid[0].size();
+        int fresh = 0;
+        queue<pair<int, int>> q;
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            if (grid[i][j] == 2) {
-                // push into queue
-                q.push({i, j});
-            } else if (grid[i][j] == 1) {
-                fresh++;
+        for (int i = 0; i < r; i++)
+            for (int j = 0; j < c; j++) {
+                if (grid[i][j] == 2)
+                    q.push({i, j});
+
+                if (grid[i][j] == 1)
+                    fresh++;
             }
-        }
-    }
-    if (fresh == 0)
-        return 0;
-    // start bfs trav
-    while (!q.empty()) {
-        int size_q = q.size();
-        int temp = 0;
-        while (size_q) {
-            pair<int, int> p = q.front();
-            q.pop();
-            int x1 = p.first;
-            int y1 = p.second;
-            int ax[4] = {1, -1, 0, 0};
-            int ay[4] = {0, 0, 1, -1};
-            for (int i = 0; i < 4; i++) {
-                int x = ax[i] + x1;
-                int y = ay[i] + y1;
 
-                if (isValid(x, y, n, m, grid)) {
-                    temp++;
-                    grid[x][y] = 2;
-                    q.push({x, y});
+        if (fresh == 0) {
+            return 0;
+        }
+        int timer = 0;
+        while (!q.empty()) {
+            timer++;
+            int curr_fresh = q.size();
+            while (curr_fresh--) {
+                int i = q.front().first;
+                int j = q.front().second;
+                q.pop();
+                for (int k = 0; k < 4; k++) {
+                    if (isValid(i + row[k], j + col[k]) &&
+                        grid[i + row[k]][j + col[k]] == 1) {
+                        grid[i + row[k]][j + col[k]] = 2;
+                        q.push({i + row[k], j + col[k]});
+                    }
                 }
             }
-            size_q--;
         }
-        if (temp != 0) {
-            time++;
-        }
-    }
+        for (int i = 0; i < r; i++)
+            for (int j = 0; j < c; j++)
+                if (grid[i][j] == 1)
+                    return -1;
 
-    // check if there is any fresh orange after bfs
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            if (grid[i][j] == 1) {
-                time = 0;
-                break;
-            }
-        }
+        return timer - 1;
     }
-
-    return (time == 0) ? -1 : time;
-}
-}
-;
+};
