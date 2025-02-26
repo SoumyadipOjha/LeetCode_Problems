f1fc3e987984ff92e4b58f8693d6f1f54e56468c
SoumyadipOjha <soumyadip.ojha@lpu.in>
2025-02-26 15:41:46 +0530

    Time: 34 ms (6.04%) | Memory: 42.2 MB (16.80%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> pre(n, 1), suf(n, 1), ans(n, 1);
-
-        // Compute prefix products
-        pre[0] = nums[0];
-        for (int i = 1; i < n; i++) {
-            pre[i] = pre[i - 1] * nums[i];
+        int n=nums.size();
+        // calculate suffix multiplications
+        vector<int>suf(n,1);
+        suf[0]=nums[0];
+        for(int i=1;i<nums.size();i++){
+            suf[i]=suf[i-1]*nums[i];
+        }
+        // calculate prefix multiplications
+        vector<int>pre(n,1);
+        pre[n-1]=nums[n-1];
+        for(int i=n-2;i>=0;i--){
+            pre[i]=pre[i+1]*nums[i];
         }
 
-        // Compute suffix products
-        suf[n - 1] = nums[n - 1];
-        for (int i = n - 2; i >= 0; i--) {
-            suf[i] = suf[i + 1] * nums[i];
+        // store in an answer array
+        vector<int>ans(n,1);
+        for(int i=0;i<n;i++){
+            int leftMul=(i>0)?suf[i-1]:1;
+            int rightMul=(i<n-1)?pre[i+1]:1;
+            ans[i]=leftMul*rightMul;
         }
-
-        // Compute result using prefix and suffix products
-        for (int i = 0; i < n; i++) {
-            int left = (i > 0) ? pre[i - 1] : 1;
-            int right = (i < n - 1) ? suf[i + 1] : 1;
-            ans[i] = left * right;
-        }
-
         return ans;
     }
 };
