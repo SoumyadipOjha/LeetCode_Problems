eb52086cc3c0f3b36f6eef32193393f87515ac75
SoumyadipOjha <soumyadip.ojha@lpu.in>
2025-02-28 01:38:01 +0530

    Time: 0 ms (100.00%) | Memory: 17.4 MB (39.20%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -2,25 +2,27 @@ class Solution {
 public:
     bool isBipartite(vector<vector<int>>& graph) {
         int n=graph.size();
-
-        vector<int>Col(n,-1);
+        vector<int>col(n,-1);
         queue<int>q;
 
+        // Step 1: check kro agar koi v colour nahi h to 0 colour kro and q m dalo
         for(int i=0;i<n;i++){
-            if(Col[i]==-1){
-                Col[i]=0;
+            if(col[i]==-1){
+                col[i]=0;
                 q.push(i);
                 while(!q.empty()){
+                    // q se nikalo and check kro 
                     int node=q.front();
                     q.pop();
+                     // check for neighbours
                     for(int j=0;j<graph[node].size();j++){
-                        // colour not assigned
-                        if(Col[graph[node][j]]==-1){
-                        Col[graph[node][j]]=(Col[node]+1)%2;
-                        q.push(graph[node][j]); 
+                        // ya to neighbour not coloured hoga
+                        if(col[graph[node][j]]==-1){
+                            col[graph[node][j]]=(col[node]+1)%2;
+                            q.push(graph[node][j]);
                         }
-                        // colour assigned
-                        if(Col[graph[node][j]]==Col[node])return 0;
+                        // ya to already coloured hoga 
+                        if(col[graph[node][j]]==col[node])return 0; 
                     }
                 }
             }
