f2b45fdf20f7dae146aa16d615e7a26d00824dc8
SoumyadipOjha <soumyadip.ojha@lpu.in>
2025-02-27 23:02:43 +0530

    Time: 28 ms (51.12%) | Memory: 18.3 MB (41.80%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,33 +1,38 @@
 class Solution {
 public:
-    int numIslands(vector<vector<char>>& grid) {
-        if (grid.empty() || grid[0].empty())
-            return 0;
-
-        int rows = grid.size();
-        int cols = grid[0].size();
-        int islands = 0;
-
-        function<void(int, int)> dfs = [&](int row, int col) {
-            if (row < 0 || col < 0 || row >= rows || col >= cols ||
-                grid[row][col] != '1')
-                return;
-            grid[row][col] = '0';
-            dfs(row - 1, col);
-            dfs(row + 1, col);
-            dfs(row, col - 1);
-            dfs(row, col + 1);
-        };
-
-        for (int row = 0; row < rows; row++) {
-            for (int col = 0; col < cols; col++) {
-                if (grid[row][col] == '1') {
-                    dfs(row, col);
-                    islands++;
+    int r, c;
+    int row[4] = {-1, +1, 0, 0};
+    int col[4] = {0, 0, -1, +1};
+    bool isValid(int i, int j){
+        return (i>=0 && i<r && j>=0 && j<c);
+    }
+    int numIslands(vector<vector<char>>& isConnected) {
+        r = isConnected.size();
+        c = isConnected[0].size();
+        queue<pair<int, int>> q;
+        int count = 0;
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if (isConnected[i][j] == '1') {
+                    count++;
+                    isConnected[i][j] = '0';
+                    q.push({i, j});
+                    while (!q.empty()) {
+                        int new_i = q.front().first;
+                        int new_j = q.front().second;
+                        q.pop();
+                        for (int k = 0; k < 4; k++) {
+                            if (isValid(new_i + row[k], new_j + col[k]) &&
+                                isConnected[new_i + row[k]][new_j + col[k]] ==
+                                    '1') {
+                                isConnected[new_i + row[k]][new_j + col[k]] = '0';
+                                q.push({new_i + row[k], new_j + col[k]});
+                            }
+                        }
+                    }
                 }
             }
         }
-
-        return islands;
+        return count;
     }
 };
