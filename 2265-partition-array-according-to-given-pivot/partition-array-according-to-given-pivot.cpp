ce27b739110b02e462e8e37bec8ce761db3c7a33
SoumyadipOjha <soumyadip.ojha@lpu.in>
2025-03-03 13:47:08 +0530

    Time: 2 ms (90.85%) | Memory: 133 MB (68.24%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         int n = nums.size();
         vector<int> ans;
         for (int i = 0; i < n; i++)
