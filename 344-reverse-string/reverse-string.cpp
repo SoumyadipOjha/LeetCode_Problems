3e1e4c4576a20c00a9c016854586631a11d8f305
SoumyadipOjha <soumyadip.ojha@lpu.in>
2025-07-06 21:00:58 +0530

    Time: 0 ms (100.00%) | Memory: 27.3 MB (16.81%) - LeetSync

--- 344-reverse-string/reverse-string.cpp
+++ 344-reverse-string/reverse-string.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        reverse(s.begin(),s.end());
+       int left=0;
+       int right=s.size()-1;
+       while(left<right){
+            swap(s[left],s[right]);
+            left++;
+            right--;
+       }
     }
 };
