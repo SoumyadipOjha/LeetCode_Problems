ab69f0d89cd25bbb5f4fb586654dbc39ed1f2aab
SoumyadipOjha <soumyadip.ojha@lpu.in>
2026-04-19 00:42:41 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (86.21%) - LeetSync

---
+++ 4168-mirror-distance-of-an-integer/mirror-distance-of-an-integer.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+int reversee(int n){
+    int ans=0;
+    while(n>0){
+        int rem=n%10;
+        ans=ans*10+rem;
+        n=n/10;
+        
+    }
+    return ans;
+    
+}
+    int mirrorDistance(int n) {
+        int rev=reversee(n);
+        cout<<rev<<" ";
+        return abs(rev-n);
+        }
+};
