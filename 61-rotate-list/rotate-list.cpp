3e3e3b1f32189c0d09961c7ac42269c33b2eb1ea
SoumyadipOjha <soumyadip.ojha@lpu.in>
2026-05-05 15:27:39 +0530

    Time: 0 ms (100.00%) | Memory: 16.3 MB (63.05%) - LeetSync

--- 61-rotate-list/rotate-list.cpp
+++ 61-rotate-list/rotate-list.cpp
@@ -1,47 +1,32 @@
 class Solution {
 public:
-    ListNode* reverseFirstK(ListNode* head, int k) {
-        if (!head || k <= 1) return head;
-
-        ListNode *prev = nullptr, *curr = head, *next = nullptr;
-        int count = 0;
-
-        while (curr != nullptr && count < k) {
-            next = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = next;
-            count++;
-        }
-
-        if (head != nullptr) {
-            head->next = curr;
-        }
-
-        return prev; 
-    }
-
     ListNode* rotateRight(ListNode* head, int k) {
         if (!head || !head->next || k == 0) return head;
 
-        int size = 0;
-        ListNode* temp = head;
-        while (temp) {
+        ListNode* tail = head;
+        int size = 1;
+
+        // get correct tail and size
+        while (tail->next) {
+            tail = tail->next;
             size++;
-            temp = temp->next;
         }
 
         k = k % size;
         if (k == 0) return head;
 
-        head = reverseFirstK(head, size);
-        ListNode* firstPart = reverseFirstK(head, k);
-        ListNode* curr = firstPart;
-        for (int i = 1; i < k; i++) {
-            curr = curr->next;
-        }
-        curr->next = reverseFirstK(curr->next, size - k);
+        // make circular
+        tail->next = head;
 
-        return firstPart;
+        ListNode* newTail = head;
+
+        for (int i = 0; i < size - k - 1; i++) {
+            newTail = newTail->next;
+        }
+
+        ListNode* newHead = newTail->next;
+        newTail->next = NULL;
+
+        return newHead;
     }
 };
